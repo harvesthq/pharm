ca983276aa1be83f71adca1402c400cbc79eeb66
Barry Hess <barry@bjhess.com>
2009-01-18 10:14:29 -0600

    Add exception notification plugin.

--- config/environment.rb
+++ config/environment.rb
@@ -83,3 +83,6 @@ Rails::Initializer.run do |config|
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 end
+
+ExceptionNotifier.sender_address       = %("Pharm bug" <info@iridesco.com>)
+ExceptionNotifier.exception_recipients = %w(shawn@iridesco.com barry@iridesco.com)
