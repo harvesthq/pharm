6fb39028034e33abff374ef5edf5cf8461276a6f
Barry Hess <barry@bjhess.com>
2009-01-23 08:54:37 -0600

    Add concerned with inititializer.

---
+++ config/initializers/concerned_with.rb
@@ -0,0 +1,10 @@
+##
+# Syntactic sugar provided by: http://paulbarry.com/articles/2008/08/30/concerned-with-skinny-controller-skinny-model
+#
+class << ActiveRecord::Base
+  def concerned_with(*concerns)
+    concerns.each do |concern|
+      require_dependency "#{name.underscore}/#{concern}"
+    end
+  end
+end
