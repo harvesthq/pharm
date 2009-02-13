7bbbe65686488e9a39427e6eec6696a4ec259101
Barry Hess <barry@bjhess.com>
2009-02-13 13:36:38 -0600

    Default / route.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,10 @@
 class PhotosController < ApplicationController
   before_filter :load_photo, :only => :show
+
+  def home
+    @photo = Photo.ordered.limited(1).first
+    render :action => :show
+  end
   
   def show; end
 
