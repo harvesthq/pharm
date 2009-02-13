de5c8b596787da9416e1b20212e401ee793b48dd
Barry Hess <barry@bjhess.com>
2009-02-13 10:11:50 -0600

    Enable edit photo screen.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,17 +1,24 @@
 class PhotosController < ApplicationController
+  before_filter :load_photo, :only => [:show, :edit, :update, :destroy]
 
   def index
     @photos = Photo.ordered
   end
   
-  def show
-    @photo = Photo.find(params[:id])
-  end
+  def show; end
   
   def new; end
     
   def create
     photo = Photo.create(params[:photo])
     redirect_to photo_path(photo)
+  end
+  
+  def edit; end
+  
+private
+
+  def load_photo
+    @photo = Photo.find(params[:id])
   end
 end
