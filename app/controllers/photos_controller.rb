d6f902addf59c08d8f0c2c661ee0a24678f263c9
Barry Hess <barry@bjhess.com>
2009-02-14 22:43:43 -0600

    Revert back from stupid stale? show call for photos.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,13 +4,9 @@ class PhotosController < ApplicationController
 
   def home
     @photo = Photo.ordered.limited(1).first
-    show
+    render :action => :show
   end
   
-  def show
-    if stale?(:etag => @photo, :last_modified => [@photo.updated_at.utc, @user.updated_at.utc].sort.last)
-      render :action => :show
-    end
-  end
+  def show; end
 
 end
