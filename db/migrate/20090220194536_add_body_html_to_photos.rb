fc1d267a10aefd56bb32271dff8ed33c7be6c938
Barry Hess <barry@bjhess.com>
2009-02-20 14:00:58 -0600

    Update body_html migration to default body_html to whatever was in body.

--- db/migrate/20090220194536_add_body_html_to_photos.rb
+++ db/migrate/20090220194536_add_body_html_to_photos.rb
@@ -1,6 +1,10 @@
 class AddBodyHtmlToPhotos < ActiveRecord::Migration
   def self.up
     add_column :photos, :body_html, :text
+    Photo.find(:all).each do |photo|
+      photo.body_html = photo.body
+      photo.save!
+    end
   end
 
   def self.down
